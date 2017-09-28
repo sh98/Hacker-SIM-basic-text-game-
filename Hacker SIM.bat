2be67c8430eb5d584395f0eed0d34c1a3e14968b
John <32228851+sh98@users.noreply.github.com>
2017-09-28 18:04:03 +0100

    Added Hacker SIM.bat

---
+++ Hacker SIM.bat
@@ -0,0 +1,44 @@
+@echo off
+color 2
+title Hacker Simulator text game by Person
+set name= 1
+echo Welcome to Hacker Simulator! In this text game, you will learn how to be a hacker!
+echo First, what is your name?
+set /p name=
+echo Is it %name% I am speaking to?
+echo Well yes, yes it is!
+echo First we need to dail up our modem.
+echo What do you want to connect to the internet with? We recommend dail up!
+echo Type the way you want to connect with:
+set /p name=
+echo You are going to use %name% to connect to the internet! Good choice!
+echo Now that you're connected to the internet, let's downloald Kali Linux!
+pause
+echo How long are you? Because, if you're a robot I can't arrest you!
+echo Please type how long you are:
+set /p name=
+echo You are %name% long... Good to know. Moving on...
+pause
+echo Now downloading Kali Linux...
+pause
+color 8
+echo SUCCESS!
+color 2
+echo Now let's hack some people on CoWFC!
+pause
+echo Nevermind, we were banned by Bean.
+echo Bean banned me guys....
+echo Just kidding... we're not going to hack people on Mario Kart!
+pause
+echo Switching to Dail-Up...
+pause
+echo Downloading random data...
+pause
+echo Still downloading random data...
+pause
+echo ok, I give up!
+pause
+echo Thank you for trying Hacker Simulator, coded and written by Person!
+echo Thank you.
+pause
+exit
